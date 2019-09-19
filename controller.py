169860422003460acae0f932020a1058306cd6fd
julian Stobbe <julian-stobbe@gmx.de>
2019-09-19 09:32:36 +0200

    opt structure

--- controller.py
+++ controller.py
@@ -1,11 +1,23 @@
-from basic_controller.controller import MetaController, SerialController
+from typing import Set, Any
+
+from arduino_controller.basicboard.board import ArduinoBoard
+
+try:
+    import Controller as ctrl
+except:
+    import basic_controller.controller as ctrl
 
 
-class Controller_GeneralController(MetaController, SerialController):
+class Controller_GeneralController(ctrl.MetaController, ctrl.SerialController):
+    required_boards = ["AnyBoard"]
+
     def __init__(self):
         super().__init__()
+        self.serial_boards: Set[ArduinoBoard] = set()
+
+    def board_added(self, board):
+        self.link_board(board)
 
 
 if __name__ == "__main__":
     a = Controller_GeneralController()
-    print(a.required_boards, a.required_controller)
