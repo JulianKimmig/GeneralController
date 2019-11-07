af54766cfbdbea13a688d4af0a22d8fd686f02a8
julian Stobbe <julian-stobbe@gmx.de>
2019-11-07 18:04:59 +0100

    no serial controller

--- controller.py
+++ controller.py
@@ -2,21 +2,15 @@ from typing import Set, Any
 
 from modular_arduino_controller.board.board import ArduinoBoard
 
-try:
-    import Controller as ctrl
-except:
-    import basic_controller.controller as ctrl
+import basic_controller.controller as ctrl
 
 
-class Controller_GeneralController(ctrl.MetaController, ctrl.SerialController):
+class Controller_GeneralController(ctrl.MetaController):
     required_boards = []
 
     def __init__(self):
         super().__init__()
         self.serial_boards: Set[ArduinoBoard] = set()
-
-    def board_added(self, board):
-        self.link_board(board)
 
 
 if __name__ == "__main__":
