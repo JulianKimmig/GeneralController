aa10b3eb69fb5ba32e3d34c6e8ebb15b0eb006a2
julian Stobbe <julian-stobbe@gmx.de>
2019-12-04 13:39:17 +0100

    blacked

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Set, Any
 
 from modular_arduino_controller.board.board import ArduinoBoard
@@ -8,10 +9,36 @@ import basic_controller.controller as ctrl
 class Controller_GeneralController(ctrl.MetaController):
     def __init__(self):
         super().__init__()
-        self.required_controller = ctrl.AyWT("BoardController")
+        self.required_controller = [
+            "BoardController",
+            "BoardController",
+            "BoardController",
+        ]
 
-    def possible_controller_added(self,controller):
-        print("HERE",self.possible_controller)
+        for i in range(len(self.required_controller)):
+            self.add_controller_function(
+                f"get_board_fields_{i}",
+                ctrl.ControllerFunction(
+                    func=partial(self.get_board_fields, position=i), visible=False
+                ),
+            )
+
+        print(self.get_controller_functions())
+
+    def possible_controller_added(self, controller):
+        print("HERE", self.possible_controller)
+
+    @ctrl.controllefunction(level=ctrl.LEVEL["NONE"], visible=False)
+    def get_board_fields(self, position):
+        controller = self.get_linked_controller(position)
+        if controller is None:
+            return
+        return controller.get_html_controller()
+
+    def get_html(self, level=ctrl.LEVEL["NONE"]):
+        print("BBBBB", level)
+        return {}
+
 
 if __name__ == "__main__":
     a = Controller_GeneralController()
