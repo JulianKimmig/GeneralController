d88c1fa2ba8c3bcccb767e1f6e439f64fa58f2aa
julian Stobbe <julian-stobbe@gmx.de>
2019-12-16 09:54:32 +0100

    browser html requests are controllerfunctions

--- controller.py
+++ controller.py
@@ -35,96 +35,88 @@ class Controller_GeneralController(ctrl.MetaController):
             return
         return controller.get_html_controller()
 
+    @ctrl.controllefunction(level=ctrl.LEVEL["VIEW"])
     def get_html(self, level=ctrl.LEVEL["NONE"]):
-        data = super().get_html(level=level)
+        data = super().get_html(self,level=level)
         if level < ctrl.LEVEL["VIEWER"]:
             return data
 
         container_subs = []
         for position in range(len(self.required_controller)):
-            boardcontroller = self.get_linked_controller(position)
+            boardcontroller =  self.get_linked_controller(position)
             if boardcontroller is not None:
-                controller_subs = []
+                controller_subs=[]
 
                 controller_subs.append(
                     ctrl.gen_html_json(
                         node_type="div",
-                        attributes={"class": "boardcontroller_title"},
-                        text=str(boardcontroller),
+                        attributes={'class':'boardcontroller_title'},
+                        text= str(boardcontroller)
                     )
                 )
 
-                input_subs = []
+                input_subs=[]
                 for title, input in boardcontroller.get_controller_webfunctions(
-                    level=level, as_html=False
+                        level=level, as_html=False
                 ).items():
                     input_subs.append(
                         ctrl.gen_html_json(
                             node_type="div",
-                            subnodes=[
-                                ctrl.gen_html_json(
-                                    node_type="label",
-                                    subnodes=[
-                                        ctrl.gen_html_json(text=title + " ("),
-                                        ctrl.gen_html_json(
-                                            node_type="span", text=input.values[0]
-                                        ),
-                                        ctrl.gen_html_json(text=")"),
-                                    ],
-                                )
-                            ]
-                            + input.html_controller()
-                            + [
-                                ctrl.gen_html_json(
-                                    node_type="button",
-                                    text="set",
-                                    attributes={
-                                        "class": [
-                                            "btn",
-                                            "btn-primary",
-                                            "board_var_set_button",
-                                        ],
-                                        "controller_id": "{{controller_id}}",
-                                        "position": position,
-                                    },
-                                )
-                            ],
-                            attributes={
-                                "class": ["form-group", "controller_input_group"]
-                            },
+                            subnodes=
+                            [ctrl.gen_html_json(
+                                node_type="label",
+                                subnodes=[
+                                    ctrl.gen_html_json(
+                                        text=title+" (",
+                                    ),
+                                    ctrl.gen_html_json(
+                                        node_type="span",
+                                        text=input.values[0],
+                                    ),
+                                    ctrl.gen_html_json(
+                                        text=")",
+                                    ),
+                                ]
+                            )]+input.html_controller()+[ctrl.gen_html_json(
+                                node_type="button",
+                                text="set",
+                                attributes={'class':['btn', 'btn-primary', 'board_var_set_button'],'controller_id':'{{controller_id}}','position':position},
+                            )]
+                            ,
+                            attributes={'class':['form-group', 'controller_input_group']},
                         )
+
                     )
 
                 controller_subs.append(
                     ctrl.gen_html_json(
                         node_type="div",
                         subnodes=input_subs,
-                        attributes={"class": "boardcontroller_inputs"},
+                        attributes={'class':'boardcontroller_inputs'},
                     )
                 )
 
-                container_subs.append(
-                    ctrl.gen_html_json(
-                        node_type="div",
-                        subnodes=controller_subs,
-                        attributes={"class": "general_boardcontroller_set"},
-                    )
+                container_subs.append(ctrl.gen_html_json(
+                    node_type="div",
+                    subnodes=controller_subs,
+                    attributes={'class':'general_boardcontroller_set'},
+                )
                 )
 
         json_html = ctrl.gen_html_json(
             node_type="div",
             subnodes=container_subs,
-            attributes={"class": "general_boardcontroller_container"},
+            attributes={'class':'general_boardcontroller_container'},
         )
 
-        data["json_html"] = json_html
+        data['json_html'] = json_html
         return data
 
-    def set_value(self, name, value, position):
+    @ctrl.controllefunction(level=ctrl.LEVEL["EDIT"])
+    def set_value(self,name,value,position):
         board_controller = self.get_linked_controller(position)
         if board_controller is not None:
-            board_controller.set_value(name, value)
-
+            board_controller.set_value(name,value)
 
 if __name__ == "__main__":
     a = Controller_GeneralController()
